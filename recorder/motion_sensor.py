c54057fdc92a6c651b410c1891883bba14d8669b
tmkdev <tekolody@gmail.com>
2018-07-29 19:08:07 -0700

    Update for local tests

--- recorder/motion_sensor.py
+++ recorder/motion_sensor.py
@@ -7,7 +7,7 @@ from Adafruit_BNO055 import BNO055
 
 
 class MotionSensor(threading.Thread):
-    def __init__(self, serial_port='/dev/serial0'):
+    def __init__(self, serial_port='/dev/serial/by-id/usb-FTDI_FT232R_USB_UART_AH02JLPE-if00-port'):
         threading.Thread.__init__(self)
         self.serial_port = serial_port
         self.rst = rst
