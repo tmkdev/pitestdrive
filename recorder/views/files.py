7339f62e57413fcc10f984f5dbda05c515b65e94
tmkdev <tekolody@gmail.com>
2018-08-04 08:13:26 -0700

    Some more HTML/Templates to support GUI 	new file:   views/files.py 	new file:   views/templates/base.html 	new file:   views/templates/files.html

---
+++ recorder/views/files.py
@@ -0,0 +1,31 @@
+import logging
+import os
+from flask import current_app, Blueprint, render_template, request, redirect, url_for, send_from_directory
+
+
+files = Blueprint('files', __name__, url_prefix='/files', template_folder='templates')
+
+@files.route('/')
+def index():
+    fpath='/home/pi/tracklogs'
+
+    flist = sorted(os.listdir(fpath))
+    stats = [os.stat(os.path.join(fpath, x)) for x in flist]
+
+    listing = dict(zip(flist, stats))
+
+    return render_template('files.html', listing=listing)
+
+@files.route('/download/<string:filename>')
+def download(filename):
+    fpath='/home/pi/tracklogs'
+
+    return send_from_directory(fpath, filename)
+
+@files.route('/delete/<string:filename>')
+def delete(filename):
+    fpath='/home/pi/tracklogs'
+
+    os.unlink(os.path.join(fpath, filename))
+
+    return redirect(url_for('files.index'))
